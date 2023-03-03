f2cfb744e1c1ef7c78a81e9d8dca0bc6ae2cba31
roy <kimathiwaweru@gmail.com>
2023-03-04 01:44:25 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 // Your code here
 const dodger = document.getElementById("dodger");
-
-
+const leftNumbers = dodger.style.left.replace("px", "");
+const left = parseInt(leftNumbers, 10);
+function beforeEach(){
+  leftNumbers = undefined;
+  left = 0;
+}
+beforeEach();
 
 function moveDodgerLeft() {
-  const leftNumbers = dodger.style.left.replace("px", "");
-  const left = parseInt(leftNumbers, 10);
+ 
 
   if (left > 0) {
     dodger.style.left = `${left - 5}px`;
   }
 }
 function moveDodgerRight() {
-  const leftNumbers = dodger.style.left.replace("px", "");
-  const left = parseInt(leftNumbers, 10);
+ 
 
   if (left < 360) {
     dodger.style.left = `${left + 5}px`;
